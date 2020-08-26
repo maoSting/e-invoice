7284ab6ee28da9d9bea52c0912582f4382acca11
DQ <hifangxcx@fujuhaofang.com>
2020-08-26 10:12:27 +0800

    UPDATE msg base64解码

--- src/Tools/DataTransform.php
+++ src/Tools/DataTransform.php
@@ -15,7 +15,7 @@ class DataTransform {
             throw new InvalidResponseException('invalid response.', 0);
         }
         if (isset($rs['returnStateInfo']['returnCode']) && $rs['returnStateInfo']['returnCode'] != Core::RETURN_SUCC) {
-            throw new InvalidResponseException(is_string($rs['returnStateInfo']['returnMessage']) ? $rs['returnStateInfo']['returnMessage'] : null, $rs['returnStateInfo']['returnCode']);
+            throw new InvalidResponseException(is_string($rs['returnStateInfo']['returnMessage']) ? base64_decode($rs['returnStateInfo']['returnMessage']) : null, $rs['returnStateInfo']['returnCode']);
         }
         return $rs;
     }
